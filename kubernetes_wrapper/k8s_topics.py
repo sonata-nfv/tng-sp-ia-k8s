ac905fea0548a1a3da6e2bf18bbed679483ae108
Thomas Soenen <thomas.soenen@ugent.be>
2019-04-22 14:01:20 +0200

    Undo changes on the master

--- kubernetes_wrapper/k8s_topics.py
+++ kubernetes_wrapper/k8s_topics.py
@@ -42,7 +42,7 @@ CNF_DEPLOY = 'infrastructure.k8s.function.deploy'
 CNF_DEPLOY_RESPONSE = 'nbi.infrastructure.k8s.function.deploy'
 CNF_FUNCTION_REMOVE = 'infrastructure.k8s.function.remove'
 CNF_SERVICE_REMOVE = 'infrastructure.k8s.service.remove'
-CNF_PREPARE = 'infrastructure.k8s.service.network.create'
+CNF_PREPARE = 'infrastructure.k8s.service.prepare'
 CNF_CONFIGURE = 'infrastructure.k8s.function.configure'
 
 # Catalogue urls
