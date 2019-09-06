e8d6a0d561e6cc637c4342def76a26a5d00c1bda
Felipe Vicens <felipe.vicens@atos.net>
2019-09-06 16:20:38 +0200

    Updating libs
    Signed-off-by: Felipe Vicens <felipe.vicens@atos.net>

--- setup.py
+++ setup.py
@@ -66,10 +66,8 @@ setup(
     license='Apache 2.0',
 
     packages=find_packages(),
-    install_requires=['amqpstorm', 'pytest', 'PyYAML', 'requests', 'pycodestyle', 'kubernetes', 'psycopg2', 
-                      'coloredlogs', 'urllib3==1.24.2', 'importlib-metadata==0.20'],
-    setup_requires=['pytest-runner'],
-
+    install_requires=['amqpstorm', 'PyYAML', 'requests', 'pycodestyle', 'kubernetes', 'psycopg2', 
+                      'coloredlogs', 'urllib3==1.24.2'],
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
